50733fa944641be169d789b98e261eb3f69f9e3f
hugovallada <valladahugo@gmail.com>
2021-03-23 17:59:14 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/github/hugovallada/hrpayroll/entities/Worker.java
@@ -0,0 +1,18 @@
+package com.github.hugovallada.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker {
+	
+	private Long id;
+	
+	private String name;
+	
+	private Double dailyIncome;
+	
+}
