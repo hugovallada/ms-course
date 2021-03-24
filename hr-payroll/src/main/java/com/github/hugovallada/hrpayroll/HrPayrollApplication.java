0fd7563a28578bee35bf01bf4da8e2a0aed4d9f4
hugovallada <valladahugo@gmail.com>
2021-03-23 23:17:19 -0300

    Feign client

--- hr-payroll/src/main/java/com/github/hugovallada/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/github/hugovallada/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package com.github.hugovallada.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // permite q o projeto use Feign
 @SpringBootApplication
 public class HrPayrollApplication {
 
---
+++ hr-payroll/src/main/java/com/github/hugovallada/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,19 @@
+package com.github.hugovallada.hrpayroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.github.hugovallada.hrpayroll.entities.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+	
+	// a interface seria a msm coisa q o método do controller
+	@GetMapping("/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+	
+}
